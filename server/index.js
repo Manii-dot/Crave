40b695d5628f74e396ceb29164bde851fc77bd40
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-12-15 18:56:10 -0600

    importing all connection in the index.js including ATlas connection

--- server/index.js
+++ server/index.js
@@ -3,3 +3,12 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//Connection to Cluster Atlas
+const CONNECTION_URL = 'mongodb+srv://sybrigcool:sybrigcool123@cluster0.fkbvv.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
