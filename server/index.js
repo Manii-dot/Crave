7416741e92596ba8fe38e2c50b1946a9596d61ce
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-12-15 19:07:54 -0600

    coonection to mongoose is good

--- server/index.js
+++ server/index.js
@@ -11,4 +11,10 @@ app.use(cors());
 
 //Connection to Cluster Atlas
 const CONNECTION_URL = 'mongodb+srv://sybrigcool:sybrigcool123@cluster0.fkbvv.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
 
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Yay this is working now on port : ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+    mongoose.set('useFindAndModify', false);
