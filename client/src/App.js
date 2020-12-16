5237f6b7c6d1581596339952bcc3302620b4ad4d
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-12-16 16:39:35 -0600

    did all importation in all component files and established post in the App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { CredentialsContainer, AppBar, Typography, Grow, Grid, Container } from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import crave from './images/crave.png';
 
 const App = () => {
     return (
         <Container maxidth="lg">
             <AppBar position="static" color="inherit">
                 <Typography variant="h2" align="center">Crave</Typography>
-                <img src={crave} alt="crave" height="60" />
+                <img src={crave} alt="crave" heigh="60" />
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     )
 }
